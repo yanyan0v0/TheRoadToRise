1c3b52fb45193da5b43655df9a88e9e4defe0e24
v_yyanliao <v_yyanliao@tencent.com>
2022-11-28 15:32:37 +0800

    fix: 初始化vue3

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,85 @@
-<template>
-  <div id="app">
-    <header>
-      <img class="logo" src="./assets/logo.png" />
-      <ul class="menu">
-        <li>首页</li>
-      </ul>
-      <div class="avatar" v-if="isLogined">
-        <img src="./assets/logo.png" alt="" srcset="">
-        <div class="avatar-right">
-          <p class="nickname">yanyan</p>
-          <span class="level">lv 1</span>
-        </div>
-      </div>
-      <div v-else class="header-login" @click="login">登录</div>
-    </header>
-    <div class="container">
-      <router-view />
-    </div>
-    <login-dialog></login-dialog>
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'App',
-  components: {
-    'login-dialog': () => import('@/components/login')
-  },
-  computed: {
-    isLogined() {
-      return this.$store.getters.isLogined
-    }
-  },
-  methods: {
-    login() {
-      this.$store.commit('setLoginType', 2)
-    }
-  }
-}
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style lang="less">
-#app{
-  display: flex;
-  flex-direction: column;
-  position: relative;
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
   width: 100%;
-  height: 100%;
-  overflow: hidden;
-  header{
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
     display: flex;
-    justify-content: space-between;
-    height: 50px;
-    padding: 5px 10%;
-    background-color: #393D49;
-    color: #fff;
-    .logo{
-      height: 50px;
-    }
-    .menu{
-      display: flex;
-      flex: 1;
-      margin: 0 20px;
-      li{
-        line-height: 40px;
-      }
-    }
-    .avatar{
-      display: flex;
-      img{
-        width: 50px;
-        height: 50px;
-        border-radius:50%;
-      }
-      .avatar-right{
-        margin: 5px;
-        font-size: 14px;
-      }
-    }
-    .header-login{
-      line-height: 40px;
-      cursor: pointer;
-    }
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
   }
-  .container{
-    flex: 1;
-    width: 80%;
-    margin: 0 auto;
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
   }
 }
 </style>
